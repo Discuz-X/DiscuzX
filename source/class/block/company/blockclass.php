ecb0e142af91c6aa8c44192f94e92867943eb986
SprayHank <fantasyoui@gmail.com>
2013-12-05 15:48:21 +0800

    WebPower版 添加企业自定义模块

---
+++ source/class/block/company/blockclass.php
@@ -0,0 +1,11 @@
+<?php defined('IN_DISCUZ') || die('Access Denied');
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: blockclass.php 27449 2012-02-01 05:32:35Z zhangguosheng $
+ */
+
+$blockclass = array(
+	'name' => '企业类', //为此目录定义一个名字
+);
