4a4984985342295977cb37faaa0a69ec2768a1ee
SprayHank <fantasyoui@gmail.com>
2013-12-16 09:51:23 +0800

    添加服务器URL重写规则

--- source/discuz_version.php
+++ source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.1');
 	define('DISCUZ_RELEASE', '20131122');
-	define('DISCUZ_MODIFY', '20131215');//自定义版本
+	define('DISCUZ_MODIFY', '20131216');//自定义版本
 	define('DISCUZ_FIXBUG', '30000000');
 }
 
