025626c1714709fdab88a4a7d999fbfb1cb9a174
SprayHank <fantasyoui@gmail.com>
2013-12-24 00:17:29 +0800

    更新日期

--- source/discuz_version.php
+++ source/discuz_version.php
@@ -14,7 +14,7 @@ if(!defined('IN_DISCUZ')) {
 if(!defined('DISCUZ_VERSION')) {
 	define('DISCUZ_VERSION', 'X3.1');
 	define('DISCUZ_RELEASE', '20131122');
-	define('DISCUZ_MODIFY', '20131217');//自定义版本
+	define('DISCUZ_MODIFY', '20131224');//自定义版本
 	define('DISCUZ_FIXBUG', '30000000');
 }
 
